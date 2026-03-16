07488c9b2b43c3dba19a361a3a3b57d65ee8cca1
Friedel Ziegelmayer <me@dignifiedquire.com>
2026-03-16 19:19:45 +0100

    feat: update to iroh@0.97 (#20)

--- examples/ping.rs
+++ examples/ping.rs
@@ -1,11 +1,11 @@
 use anyhow::Result;
-use iroh::{Endpoint, protocol::Router};
+use iroh::{Endpoint, endpoint::presets, protocol::Router};
 use iroh_ping::Ping;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // create the receive side
-    let recv_ep = Endpoint::builder().bind().await?;
+    let recv_ep = Endpoint::bind(presets::N0).await?;
     let recv_router = Router::builder(recv_ep.clone())
         .accept(iroh_ping::ALPN, Ping::new())
         .spawn();
@@ -13,7 +13,7 @@ async fn main() -> Result<()> {
     let addr = recv_router.endpoint().addr();
 
     // create a send side & send a ping
-    let send_ep = Endpoint::builder().bind().await?;
+    let send_ep = Endpoint::bind(presets::N0).await?;
     let send_pinger = Ping::new();
     let rtt = send_pinger.ping(&send_ep, addr).await?;
     println!("ping took: {rtt:?} to complete");
--- examples/quickstart.rs
+++ examples/quickstart.rs
@@ -19,14 +19,14 @@
 use std::env;
 
 use anyhow::{Result, anyhow};
-use iroh::{Endpoint, protocol::Router};
+use iroh::{Endpoint, endpoint::presets, protocol::Router};
 use iroh_ping::Ping;
 use iroh_tickets::{Ticket, endpoint::EndpointTicket};
 
 async fn run_receiver() -> Result<()> {
     // Create an endpoint, it allows creating and accepting
     // connections in the iroh p2p world
-    let endpoint = Endpoint::bind().await?;
+    let endpoint = Endpoint::bind(presets::N0).await?;
 
     // Wait for the endpoint to be accessible by others on the internet
     endpoint.online().await;
@@ -50,7 +50,7 @@ async fn run_receiver() -> Result<()> {
 
 async fn run_sender(ticket: EndpointTicket) -> Result<()> {
     // create a send side & send a ping
-    let send_ep = Endpoint::bind().await?;
+    let send_ep = Endpoint::bind(presets::N0).await?;
     let send_pinger = Ping::new();
     let rtt = send_pinger
         .ping(&send_ep, ticket.endpoint_addr().clone())
