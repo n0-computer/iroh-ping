f459e92e4c466463005bac3918b3f85e219eedc2
dignifiedquire <dignifiedquire@users.noreply.github.com>
2026-05-27 09:48:47 +0000

    deploy: 822abc17fa0a543be19dd3bf3bac4383c4b3d8c9

---
+++ pr/24/docs/trait.impl/core/marker/trait.UnsafeUnpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_ping",[["impl UnsafeUnpin for <a class=\"struct\" href=\"iroh_ping/struct.Metrics.html\" title=\"struct iroh_ping::Metrics\">Metrics</a>",1,["iroh_ping::Metrics"]],["impl UnsafeUnpin for <a class=\"struct\" href=\"iroh_ping/struct.Ping.html\" title=\"struct iroh_ping::Ping\">Ping</a>",1,["iroh_ping::Ping"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[317]}
