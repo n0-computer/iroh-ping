32ac93f53b7fbfdc5f38371f64ccb777b25d390f
rklaehn <rklaehn@users.noreply.github.com>
2026-07-09 08:09:15 +0000

    deploy: 6bb9021aa5f542901aea9d85467816d5f64adc68

---
+++ pr/28/docs/trait.impl/iroh_metrics/base/trait.MetricsGroup.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_ping",[["impl MetricsGroup for <a class=\"struct\" href=\"iroh_ping/struct.Metrics.html\" title=\"struct iroh_ping::Metrics\">Metrics</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[148]}
