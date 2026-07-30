4f3bbd338eec6b08bbacfa91c6e811114f580850
flub <flub@users.noreply.github.com>
2026-07-30 10:42:09 +0000

    deploy: fde1074a7a12b4c456da03795bc9fbc43408785c

---
+++ pr/30/docs/trait.impl/iroh_metrics/iterable/trait.Iterable.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["iroh_ping",[["impl Iterable for <a class=\"struct\" href=\"iroh_ping/struct.Metrics.html\" title=\"struct iroh_ping::Metrics\">Metrics</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[144]}
